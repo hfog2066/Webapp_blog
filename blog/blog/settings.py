b9b580c9f98d36abe964f525a92cc77eeee51965
Hector <hfog2000@yahoo.com>
2021-05-11 14:09:17 -0500

    error corrected

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'boards.apps.BoardsConfig',
     'background_task',
     'widget_tweaks',
     'django_filters',
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards.apps.BoardsConfig',    
+        
 ]
 
 MIDDLEWARE = [
